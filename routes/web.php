8729b9b16a15d767a871e7c2d81cac6b075fdb36
axq02 <axq0267@gmail.com>
2023-07-16 14:52:42 -0500

    Fetching Tmdb Movies Data

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\TmdbmoviesController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -20,3 +21,5 @@ Route::get('/', function () {
 Route::get('/dbconn', function () {
     return view('test_db');
 });
+
+Route::get('/fetch',[tmdbmoviesController::class,'fetchMovies']);
